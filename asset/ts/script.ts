57fb7b343977b20f36ea75001c3a1cd6bdd9b53d
José Nícollas Silva de Souza <josenicollassilvadesouza@gmail.com>
2026-02-15 23:29:38 -0300

    feat: add files operations and scripts

---
+++ asset/ts/script.ts
@@ -0,0 +1,57 @@
+import { add, substract, multiply, div, divInt, remainder } from "./operations";
+
+const number1 = document.querySelector("#number1") as HTMLInputElement;
+const number2 = document.querySelector("#number2") as HTMLInputElement;
+const calculatorBtn = document.querySelector("#calculator") as HTMLButtonElement;
+const output = document.querySelector("output") as HTMLOutputElement;
+
+const operations = document.querySelector("input[name='operations']:checked") as HTMLInputElement;
+
+let valueOperations = operations.value;
+let resultad: number = 0;
+
+calculatorBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	let num1: number = number1.valueAsNumber;
+	let num2: number = number2.valueAsNumber;
+
+	switch(valueOperations) {
+		case "add": {
+			resultad = add(num1, num2);
+			break;
+		}
+
+		case "substract": {
+			resultad = substract(num1, num2);
+			break;
+		}
+
+		case "multiply": {
+			resultad = multiply(num1, num2);
+			break;
+		}
+
+		case "div": {
+			resultad = div(num1, num2);
+			break;
+		}
+
+		case "divInt": {
+			resultad = divInt(num1, num2);
+			break;
+		}
+
+		case "remainder": {
+			resultad = remainder(num1, num2);
+			break;
+		}
+
+		default: {
+			alert("Error");
+			break;
+		}
+	}
+
+	output.textContent = resultad.toString();
+});
